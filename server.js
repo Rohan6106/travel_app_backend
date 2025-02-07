2018b7d22fc4dbc75246a72c0a458dece75e9cc3
Rohan Hande <rohanhande22@gmail.com>
2025-02-07 17:18:57 +0530

    fix: resolved cors issue

--- server.js
+++ server.js
@@ -5,7 +5,6 @@
 // const hotelDataAddedToDBRouter = require("./routes/dataimport.router");
 // const categoryAddedToDBRouter = require("./routes/categoryimport.router");
 
-
 // const connectDB = require("./config/dbconfig");
 
 // const app = express();
@@ -19,7 +18,7 @@
 // ))
 
 // app.use("/api/hoteldata", hotelDataAddedToDBRouter);
-// app.use("/api/hotels",hotelRouter);    
+// app.use("/api/hotels",hotelRouter);
 // app.use("/api/categorydata", categoryAddedToDBRouter);
 
 // mongoose.connection.once("open", () => {
@@ -29,15 +28,16 @@
 //     });
 //   });
 
-
 const express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
+dotenv.config();
 
 const hotelRouter = require("./routes/hotel.router");
 const hotelDataAddedToDBRouter = require("./routes/dataimport.router");
 const categoryAddedToDBRouter = require("./routes/categoryimport.router");
-
 
 const categoryRouter = require("./routes/category.router");
 const singleHotelRouter = require("./routes/singlehotel.router");
@@ -47,27 +47,26 @@ const wishlistRouter = require("./routes/wishlist.router");
 const connectDB = require("./config/dbconfig");
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 connectDB();
 
 const PORT = 3500;
 
-app.get("/",(req,res) => (res.send("Hello MERN")
-))
+app.get("/", (req, res) => res.send("Hello MERN"));
 
 app.use("/api/hoteldata", hotelDataAddedToDBRouter);
-app.use("/api/hotels",hotelRouter);
+app.use("/api/hotels", hotelRouter);
 app.use("/api/categorydata", categoryAddedToDBRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/hotels", singleHotelRouter);
-app.use("/api/auth",authRouter);
-app.use("/api/wishlist",wishlistRouter);
-
+app.use("/api/auth", authRouter);
+app.use("/api/wishlist", wishlistRouter);
 
 mongoose.connection.once("open", () => {
-    console.log("Connected to DB");
-    app.listen(process.env.PORT || PORT, () => {
-      console.log("Server is Up and Running");
-    });
+  console.log("Connected to DB");
+  app.listen(process.env.PORT || PORT, () => {
+    console.log("Server is Up and Running");
   });
+});
